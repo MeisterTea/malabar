fd219f76b395dd0a916a6b5c07d626caf85d1162
MeisterTea <git@pasquierfrancois.com>
2019-01-07 10:34:56 +0100

    Add battery prefix fallback

--- src/battery.rs
+++ src/battery.rs
@@ -14,11 +14,14 @@ use std::{
 use crate::paint::set_label_color;
 
 const POWER_SUPPLY_ROOT: &str = "/sys/class/power_supply";
+const REFRESH_RATE: u32 = 500;
+const FALLBACK_PREFIX: &str = "charge";
 
 #[derive(Debug)]
 struct Battery {
     id: String,
     charging: bool,
+    prefix: String,
     current_charge: u32,
     full_charge: u32
 }
@@ -51,26 +54,38 @@ fn get_charge(battery_id: &String, state: &str) -> String {
         .unwrap_or_else(|_| String::from(""))
 }
 
+fn get_prefix(battery_id: &String) -> Result<String, io::Error> {
+    let path = format!("{}/{}/energy_now", POWER_SUPPLY_ROOT, battery_id);
+    match File::open(path) {
+        Ok(res) => Ok(String::from("energy")),
+        Err(e) => Err(e)
+    }
+}
+
+// TODO use /uevent ?
 pub fn init_battery() -> Rc<Label> {
     let label = Label::new(None);
     label.set_margin_end(7);
     set_label_color(&label, 255, 255, 255);
     let label_rc = Rc::new(label);
     let battery_id = &get_battery_ids()[0];
-    let current_charge = get_charge(battery_id, "energy_now").parse::<u32>()
+    let prefix = get_prefix(battery_id).unwrap_or_else(|_| String::from(FALLBACK_PREFIX));
+    let prefix_clone = prefix.to_owned();
+    let current_charge = get_charge(battery_id, &format!("{}_now", &prefix)).parse::<u32>()
         .unwrap_or_else(|_| 0 as u32);
-    let full_charge = get_charge(battery_id, "energy_full").parse::<u32>()
+    let full_charge = get_charge(battery_id, &format!("{}_full", &prefix)).parse::<u32>()
         .unwrap_or_else(|_| 0 as u32);
     let battery = Battery {
-        id: battery_id.to_string(),
-        charging: true,
+        prefix,
         current_charge,
-        full_charge
+        full_charge,
+        id: battery_id.to_string(),
+        charging: true
     };
     let label_clone = label_rc.clone();
     let battery_id_clone = battery_id.clone(); // just use lifetime of battery_id or reintroduce battery Struct ?
-    timeout_add(500, move || {
-        let current_charge = get_charge(&battery_id_clone, "energy_now").parse::<u32>()
+    timeout_add(REFRESH_RATE, move || {
+        let current_charge = get_charge(&battery_id_clone, &format!("{}_now", &prefix_clone)).parse::<u32>()
             .unwrap_or_else(|_| 0 as u32);
         let battery_percentage = format!("{:.*}%", 0, current_charge as f32 / full_charge as f32 * 100 as f32);
         label_clone.set_text(&battery_percentage);
